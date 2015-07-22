09570fa5484accf43fb763e3514e430fa5f6c5f5
Gene <im885555@gmail.com>
2015-07-22 18:03:27 +0800

    create

--- HelloCPP/HelloCPP/Advanced.h
+++ HelloCPP/HelloCPP/Advanced.h
@@ -10,4 +10,14 @@ class advanced{
 		int arrayIdiom4();
 		int stlIdiom();
 		int stlIdiom2();
+		int cStyle();
+		int stringPrac();
+		int stringPrac2();
+
+		int pointer();
+		int pointer2();
+		int pointer3();
+		int pointer4();
+		int pointer5();
+		int pointer6();
 };
--- HelloCPP/HelloCPP/Advancedcpp.cpp
+++ HelloCPP/HelloCPP/Advancedcpp.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream> 
 #include <vector>
+#include <string> 
 #include "Advanced.h" 
 using namespace std;
 
@@ -16,6 +17,9 @@ int advanced::exec(){
 	twoDimArray();
 	arrayIdiom();
 	stlIdiom();
+	cStyle();
+	stringPrac();
+	pointer();
 
 	return 0;
 }
@@ -182,8 +186,8 @@ int advanced::stlIdiom2(){
 	cout << endl;
 
 	cout << "輸入搜尋值：";
-	int search = 0;
-	cin >> search;
+	int search = 31;
+	cout << search;
 
 	vector<int>::iterator it =
 		find(ivector.begin(), ivector.end(), search);
@@ -205,5 +209,155 @@ int advanced::stlIdiom2(){
 		cout << *it << " ";
 	}
 	cout << endl;
+	return 0;
+}
+
+int advanced::cStyle(){
+	cout << "\n字元陣列（C-style 字串）\n";
+
+	char str[] = "hello";
+
+	for (int i = 0; i < (sizeof(str) / sizeof(str[0])); i++) {
+		if (str[i] == '\\0')
+			cout << " null";
+		else
+			cout << " " << str[i];
+	}
+	cout << endl;
+
+
+	char str1[80] = { '\\0' };
+	char str2[] = "caterpillar";
+
+	cout << "str1: " << str1 << endl
+		<< "str2: " << str2 << endl
+		<< endl;
+
+	// 將str2複製給str1 
+	strcpy(str1, str2);
+	cout << "str1: " << str1 << endl
+		<< "str2: " << str2 << endl
+		<< endl;
+
+	// 將str2接在str1後 
+	strcat(str1, str2);
+	cout << "str1: " << str1 << endl
+		<< "str2: " << str2 << endl
+		<< endl;
+
+	cout << "str1長度：" << strlen(str1) << endl
+		<< "str2長度：" << strlen(str2) << endl
+		<< endl;
+
+	cout << "str1與str2比較：" << strcmp(str1, str2) << endl
+		<< endl;
+
+	return 0;
+}
+
+int advanced::stringPrac(){
+	cout << "\n使用 string 型態\n";
+
+	
+	string str1;
+	string str2("caterpillar");
+	string str3(str2);
+
+	if (str1.empty()) {
+		cout << "str1 為空字串" << endl;
+	}
+
+	cout << "str1 長度: " << str1.size() << endl;
+	cout << "str2 長度: " << str2.size() << endl;
+	cout << "str3 長度: " << str3.size() << endl;
+
+	if (str1 == str2) {
+		cout << "str1 與 str2 內容相同" << endl;
+	}
+
+	if (str3 == str2) {
+		cout << "str3 與 str2 內容相同" << endl;
+	}
+
+	string name("caterpillar");
+
+	for (int i = 0; i < name.size(); i++) {
+		cout << name[i] << endl;
+	}
+
+	stringPrac2();
+
+	return 0;
+}
+
+int advanced::stringPrac2(){
+	string str1;
+	string str2("caterpillar");
+	string str3(str2);
+
+	// assign: 指定字串 
+	str1 = str1.assign(str2, 0, 5);
+	cout << "str1: " << str1 << endl;
+	str1 = str1.assign("caterpillar", 5, 6);
+	cout << "str1: " << str1 << endl;
+
+	str1 = "";
+
+	// append: 字串串接 
+	str1 = str1.append(str2, 0, 5);
+	str1 = str1.append(str3, 5, 6);
+	cout << "str1: " << str1 << endl;
+
+	// find: 尋找字串位置 
+	cout << "尋找str1中的第一個pill: "
+		<< str1.find("pill", 0) << endl;
+
+	// insert: 插入字串 
+	cout << "在str1插入字串***: "
+		<< str1.insert(5, "***") << endl;
+
+	cout << "str1長度: " << str1.length() << endl;
+
+	return 0;
+}
+
+int advanced::pointer(){
+	cout << "\n指標與記憶體位址\n";
+
+	int var = 10;
+
+	cout << "變數var的值：" << var
+		<< endl;
+	cout << "變數var的記憶體位址：" << &var
+		<< endl;
+
+	pointer2();
+	
+	return 0;
+}
+
+int advanced::pointer2(){
+	int var = 10;
+	int *ptr = &var;
+
+	cout << "變數var的位址：" << &var
+		<< endl;
+	cout << "指標ptr指向的位址：" << ptr
+		<< endl;
+
+	pointer3();
+
+	return 0;
+}
+
+int advanced::pointer3(){
+	int var = 10;
+	int *ptr = &var;
+
+	cout << "變數var的位址：" << &var
+		<< endl;
+	cout << "指標ptr指向的位址：" << ptr
+		<< endl;
+
 	return 0;
 }
